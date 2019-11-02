43633807c406acfab82101e3c7a3e856b2edba18
Trond Arild Tjøstheim <trondarild@gmail.com>
2019-11-02 10:39:22 +0100

    added topic template, script outline

---
+++ scripts/make_topic_page.py
@@ -0,0 +1,20 @@
+#
+# chops syntopicon into discrete topics with ref list
+# and makes mediawiki pages for each topic
+#
+
+bookname = "[[Great books of the Western world volume # (book)|refname]]"
+
+# read syntopicon text
+
+# parcel into ideas
+
+# for each idea, extract references
+
+# for references, extract each topic and its references
+
+# add link to book page to each reference item
+
+# generate mediawiki page for topic
+
+# add page to a file for the current idea 
